72c5e994d672d70a5c8c3d0a3cfb7fb973a7fa89
Victor Reis <victor.sreis@hotmail.com>
2023-06-08 12:22:56 -0300

    Listar médico

--- src/main/java/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,15 @@
 package com.med.voll.api.controller;
 
 import com.med.voll.api.dto.MedicoRequest;
+import com.med.voll.api.dto.MedicoResponse;
 import com.med.voll.api.model.Medico;
 import com.med.voll.api.repository.MedicoRepository;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,7 +22,18 @@ public class MedicoController {
     }
 
     @PostMapping
-    public ResponseEntity cadastrarMedico(@RequestBody @Valid MedicoRequest medicoRequest){
-    return ResponseEntity.ok(medicoRepository.save(new Medico(medicoRequest))) ;
+    public ResponseEntity cadastrarMedico(@RequestBody @Valid MedicoRequest medicoRequest) {
+        Medico medico = medicoRepository.save(new Medico(medicoRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new MedicoResponse(medico));
+    }
+
+    @GetMapping
+    public ResponseEntity getAllMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(medicoRepository.findAll(pageable).map(MedicoResponse::new));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getMedico(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(medicoRepository.findById(id));
     }
 }
---
+++ src/main/java/com/med/voll/api/dto/MedicoResponse.java
@@ -0,0 +1,27 @@
+package com.med.voll.api.dto;
+
+import com.med.voll.api.model.Especialidade;
+import com.med.voll.api.model.Medico;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record MedicoResponse(
+        Long id,
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade
+) {
+
+    public MedicoResponse(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
